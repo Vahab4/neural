c01a8fc5a49d372919fa169a8d5b3851ef9e336e
Vahab4 <vahab.asadizadeh@gmail.com>
2025-03-03 17:06:58 +0300

    alllllllll

--- hygiene colloqium robat.py
+++ hygiene colloqium robat.py
@@ -1,42 +1,31 @@
-from flask import Flask, render_template, request, jsonify
 import json
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, template_folder='', static_folder='./static')
+app = Flask(__name__, static_folder='./static')
 
-# Функция для чтения данных из JSON-файла и загрузки их в словарь
-def load_from_json(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
-        return {}  # Возвращаем пустой словарь, если файл не найден
-
-# Загружаем данные из JSON-файла в словарь
-d = load_from_json('data.json')
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        q = request.form.get('question')
-        a = request.form.get('answer')
-        if q and a:
-            d[q] = a
-            save_to_json()
-            return jsonify(message=f"Вопрос '{q}' и ответ '{a}' успешно добавлены!")
-        else:
-            return jsonify(message="Пожалуйста, введите вопрос и ответ.")
-    return render_template('index.html', data=d)
+    return send_from_directory('.', 'index.html')
+
+@app.route('/add_qa', methods=['POST'])
+def add_qa():
+    q = request.form.get('question')
+    a = request.form.get('answer')
+    if q and a:
+        d[q] = a
+        save_to_json()
+        return jsonify(message=f"Вопрос '{q}' и ответ '{a}' успешно добавлены!")
+    else:
+        return jsonify(message="Пожалуйста, введите вопрос и ответ.")
+
+@app.route('/show_data', methods=['GET'])
+def show_data():
+    return jsonify(d)
 
 def save_to_json():
     with open('src/data.json', 'w', encoding='utf-8') as file:
         json.dump(d, file, ensure_ascii=False, indent=4)
 
-@app.route('/show', methods=['GET'])
-def show_data():
-    return jsonify(d)
-
 if __name__ == '__main__':
     app.run(debug=True)
-
     
